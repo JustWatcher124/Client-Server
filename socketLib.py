3334ea0dea196e4581399b8286d77e93d4c5d63e
JustWatcher124 <60944288+JustWatcher124@users.noreply.github.com>
2022-02-03 14:14:46 +0100

    Add files via upload
    generelle socket-funktionen

---
+++ socketLib.py
@@ -0,0 +1,27 @@
+
+def empfangeStr(komm_s):
+    weiter = True
+    datenBytes = bytes()
+
+    endByte = bytes([0])
+
+    while weiter:
+        chunk = komm_s.recv(1)
+        if chunk == endByte or chunk == bytes([]):
+            weiter = False
+        else:
+            datenBytes = datenBytes + chunk
+
+    datenStr = str(datenBytes, 'utf-8')
+
+    return datenStr
+
+
+def sendeStr(komm_s, datenStr):
+    datenBytes = bytes(datenStr, 'utf-8')
+    komm_s.sendall(datenBytes)
+
+
+def sendeTrennByte(komm_s):
+    trennByte = bytes([0])
+    komm_s.sendall(trennByte)
