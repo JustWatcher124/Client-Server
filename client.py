4d2f63d414ec18e908281b6d52e0299198edfae0
JustWatcher124 <60944288+JustWatcher124@users.noreply.github.com>
2022-02-10 09:32:03 +0100

    commented

--- client.py
+++ client.py
@@ -4,19 +4,26 @@ import socketLib
 import socket
 import chatGui
 import auswahlGui
+# Hauptprogramm für den Client
+# Beinhaltet client, nickname und serverip
+
 print('''Willkommen bei unserem super-tollem-Nachrichtensystem,
 bevor es losgehen kann, brauchen Wir die IP des Servers an.
 (Es sollte sich ein neues Fenster geöffnet haben)''')
 
-
+# Client wird als socket.socket instanziiert
 client = socket.socket()
-client.settimeout(10)
+client.settimeout(10) # Sockettimeout = Zeit bis der Client nach Verbindungsabbruch einen Fehler bringt
 try:
     nickname = nameGetGui.main()
+    if nickname = "":
+        raise NickNameError()
     ip,port = serverIP_Eingabe.getIP().split(":")
+except NickNameError:
+    print("Sie haben das Fenster Geschlossen")
 except:
     print("Sie haben Abbrechen gedrückt")
-print(nickname, "Im client")
+print("Sie sind als",nickname,"angemeldet")
 print(ip,port)
 try:
     client.connect((ip,int(port)))
@@ -29,6 +36,8 @@ except:
 else:
     print("Alles Gut")
     auswahlGui.starteGui(client,nickname)
+# Client schickt eine 0 an Server um Trennen der Verbindung zu signalisieren
 socketLib.sendeStr(client,"0");socketLib.sendeTrennByte(client)
 client.close()
 print("Schönen Tag noch")
+
