1f2bb7786e543bff285caced6dd369e4a23bcb9a
JustWatcher124 <60944288+JustWatcher124@users.noreply.github.com>
2022-02-10 09:31:32 +0100

    commented

--- chatGui.py
+++ chatGui.py
@@ -2,15 +2,20 @@ import tkinter as tk
 from tkinter import ttk
 from nachrichtFormatter import *
 from socketLib import *
+import auswahlGui
+# Chat Fenster mit Nachrichteneingabe
 
+
+# Funktion zum Befüllen des Fensters mit den Widgets
 def gui(fenster):
     global window
     window = fenster
     toprow = tk.Frame(window, height=50)
     chatverlauf = tk.Canvas(window, bg="#D3D3D3",height=250)
     kein_verlauf = tk.Label(toprow,text="Es sind keine Nachrichten vorhanden, drücke auf 'refresh'",width=50)
-    tk.Button(toprow,text="Schließen",command= chatSchliessen).grid(row=0,column=0)
-    tk.Button(toprow,text="refresh",command= lambda: refresh(kein_verlauf,chatverlauf)).grid(row=0,column=2)
+    tk.Button(toprow, text="Zurück", command=back_to_Auswahl).grid(row=0,column=0)
+    tk.Button(toprow,text="Schließen",command= chatSchliessen).grid(row=0,column=2)
+    tk.Button(toprow,text="Refresh",command= lambda: refresh(kein_verlauf,chatverlauf)).grid(row=0,column=4)
     bottomrow = tk.Frame(window)
     text_input = tk.Text(bottomrow, height = 1, width=50)
     tk.Button(bottomrow,text="Absenden", command= lambda: text_absenden(text_input,kein_verlauf,chatverlauf)).grid(row=2,column=1)
@@ -22,29 +27,45 @@ def gui(fenster):
     refresh(kein_verlauf,chatverlauf)
     return window
 
-
+# Funktion des Knopfen "Schließen"
 def chatSchliessen():
     global window
     window.destroy()
 
+# Funktion des Knopfes "Zurück"
+# Schließt das Chatfenster und öffnet die Chatauswahl wieder
+def back_to_Auswahl():
+    global nickname, client
+    chatSchliessen()
+    auswahlGui.starteGui(client,nickname)
+
+# Funktion des Knopfes "Absenden"
+# Liest die eingegebene Nachricht und löscht den Inhalt des Textfeldes
 def text_absenden(text,k_v,cv):
     global client, neuer_chat
     nachricht = text.get(1.0,"end-1c")
     nachricht_zum_server(nachricht)
     neuer_chat = False
+    text.delete("1.0","end-1c"
     refresh(k_v,cv)
 
+# Funktion unter text_absenden()
+# schickt dem Server die Nachricht im json-string Format
 def nachricht_zum_server(nachricht):
     global nickname, komm_partner
     raw = nachrichtTjson(nickname,komm_partner,nachricht)
     sendeStr(client,"6"+raw); sendeTrennByte(client)
 
 
+# Funktion unter refresh()
+# Löscht den Inhalt eines tk_Frames/Windows
+# Damit Nachrichten nicht mehrfach angezeigt werden
 def clear_widgets(tk_frame):
     for widget in tk_frame.winfo_children():
         widget.destroy()
 
-
+# Funktion des Knopfes "Refresh"
+# Empfängt den Chatverlauf vom Server und übergibt ihn an make_chatVerlauf()
 def refresh(kein_verlauf,chat_verlauf):
     global client, komm_partner, neuer_chat
     clear_widgets(chat_verlauf)
@@ -58,7 +79,8 @@ def refresh(kein_verlauf,chat_verlauf):
     else:
         kein_verlauf.config(text="Es gibt keine Nachrichten für diesen Kontakt")
 
-
+# Funktion unter refresh()
+# Um das Layout der Nachrichten zu machen und diese anzuzeigen 
 def make_chatVerlauf(anzeige,verlauf_raw):
     global nickname, partner
     if verlauf_raw == '':
@@ -75,6 +97,8 @@ def make_chatVerlauf(anzeige,verlauf_raw):
             tk.Label(anzeige,text=mes,bg=seite[1]).grid(column=seite[0])
 
 
+# Anfang des Fensters
+# Beinhaltet das Fenster dessen mainloop
 def starteGui(client_socket,nick,partner, new_chat):
     global client, nickname, komm_partner, neuer_chat
     client = client_socket
@@ -85,4 +109,5 @@ def starteGui(client_socket,nick,partner, new_chat):
     app.title("Ihr Chat mit:"+komm_partner)
     app = gui(app)
     app.mainloop()
+
 
